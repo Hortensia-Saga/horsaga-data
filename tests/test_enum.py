d7c3cf38aa6c482b37d03113a832806c445d5381
Abel Cheung <abelcheung@gmail.com>
2022-01-22 02:02:03 +0000

    New enum tests has to be done at file level and excluded for py38, otherwise new language feature would cause pytest to fail early at test collection stage

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -2,14 +2,9 @@
 # Test: EnumMultiValueMixin works for enums
 #
 
-import enum
-import sys
-from collections import namedtuple
 from random import choice
 
 import pytest
-
-from horsaga.data import EnumMultiValueMixin
 
 
 def test_attr_roundtrip():
@@ -55,40 +50,3 @@ def test_attr_chr_re(field, expected):
 def test_attr_alt_re(field, expected):
     from horsaga.data import AtkAttr
     assert AtkAttr.alternation_re(field) == expected
-
-#
-# Test: EnumMultiValueMixin with whitelisted / ignored fields
-#
-
-_fields = namedtuple('_MyFields', ('attr1', 'attr2', 'attr3', 'attr4'))
-
-class ModFour(EnumMultiValueMixin, _fields, enum.Enum, lookup_enable=('attr1', 'attr3')):
-    ZERO  = (0, 4,  8, 'doh')
-    ONE   = (1, 5,  9, 'doh')
-    TWO   = (2, 6, 10, 'doh')
-    THREE = (3, 7, 11, 'doh')
-
-@pytest.mark.skipif(sys.version_info < (3, 9, 2), reason='Feature requires Py 3.9.2+')
-def test_multivalue_include():
-    assert ModFour(1) == ModFour.ONE
-    assert ModFour(9) == ModFour.ONE
-    with pytest.raises(ValueError):
-        ModFour(5)
-    with pytest.raises(ValueError):
-        ModFour('doh')
-
-class ModFourAlt(EnumMultiValueMixin, _fields, enum.Enum, lookup_disable=('attr3', 'junk')):
-    ZERO  = (0, 4,  8, 'doh')
-    ONE   = (1, 5,  9, 'doh')
-    TWO   = (2, 6, 10, 'doh')
-    THREE = (3, 7, 11, 'doh')
-
-@pytest.mark.skipif(sys.version_info < (3, 9, 2), reason='Feature requires Py 3.9.2+')
-def test_multivalue_exclude():
-    assert ModFourAlt(1) == ModFourAlt.ONE
-    assert ModFourAlt(5) == ModFourAlt.ONE
-    with pytest.raises(ValueError):
-        ModFourAlt(9)
-    # Last enum member wins, but we don't depend on such behavior
-    # Generally such fields should be excluded instead
-    assert isinstance(ModFourAlt('doh'), ModFourAlt)
---
+++ tests/test_enum_py39.py
@@ -0,0 +1,43 @@
+
+#
+# Test: EnumMultiValueMixin with whitelisted / ignored fields
+# Excluded for python < 3.9
+#
+
+import enum
+import pytest
+from collections import namedtuple
+
+from horsaga.data import EnumMultiValueMixin
+
+
+_fields = namedtuple('_MyFields', ('attr1', 'attr2', 'attr3', 'attr4'))
+
+class ModFour(EnumMultiValueMixin, _fields, enum.Enum, lookup_enable=('attr1', 'attr3')):
+    ZERO  = (0, 4,  8, 'doh')
+    ONE   = (1, 5,  9, 'doh')
+    TWO   = (2, 6, 10, 'doh')
+    THREE = (3, 7, 11, 'doh')
+
+def test_multivalue_include():
+    assert ModFour(1) == ModFour.ONE
+    assert ModFour(9) == ModFour.ONE
+    with pytest.raises(ValueError):
+        ModFour(5)
+    with pytest.raises(ValueError):
+        ModFour('doh')
+
+class ModFourAlt(EnumMultiValueMixin, _fields, enum.Enum, lookup_disable=('attr3', 'junk')):
+    ZERO  = (0, 4,  8, 'doh')
+    ONE   = (1, 5,  9, 'doh')
+    TWO   = (2, 6, 10, 'doh')
+    THREE = (3, 7, 11, 'doh')
+
+def test_multivalue_exclude():
+    assert ModFourAlt(1) == ModFourAlt.ONE
+    assert ModFourAlt(5) == ModFourAlt.ONE
+    with pytest.raises(ValueError):
+        ModFourAlt(9)
+    # Last enum member wins, but we don't depend on such behavior
+    # Generally such fields should be excluded instead
+    assert isinstance(ModFourAlt('doh'), ModFourAlt)
